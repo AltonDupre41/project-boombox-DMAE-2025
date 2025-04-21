0d35f6ef222109a71b4636b142672a865c801a70
Alton Dupre <altondupre@eatel.net>
2025-04-21 15:17:28 -0500

    additional changes and fixes

--- team-boombox-project/src/Room.gd
+++ team-boombox-project/src/Room.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
-@export var dialogSignal = "-1"
+@export var dialogSignal = "1"
 
 func _ready() -> void:
 	if dialogSignal != "-1":
+		JavaScriptBridge.eval("console.log(\"Dialouge Sent!\");",true)
 		JavaScriptBridge.eval(str("triggerDialogue(",dialogSignal,")"),true)
+		
 		
--- team-boombox-project/src/moving_platform.gd
+++ team-boombox-project/src/moving_platform.gd
@@ -1,6 +1,42 @@
 extends TileMapLayer
-@export var maxHeight:float = 10
-@export var speed:int = 2
+@export var maxHeight:float = 20
+@export var speed:int = 50
+@export var ON = true
+@export var x_DIR = false
+@export var BACK = false
+
+var MAXHEIGHT
+var MINHEIGHT
+
+func _ready() -> void:
+	if x_DIR: 
+		MAXHEIGHT = position.x + maxHeight
+		MINHEIGHT = position.x - maxHeight
+			
+	else: 
+		MAXHEIGHT = position.y + maxHeight
+		MINHEIGHT = position.y - maxHeight
+	
 
 func _physics_process(delta: float) -> void:
-	pass
+	if !ON: return
+	if x_DIR: 
+		position.x = move_toward(position.x,MAXHEIGHT, speed*delta)
+		if BACK && position.x <= MAXHEIGHT:
+			position.x = MAXHEIGHT - 1
+			MAXHEIGHT = MAXHEIGHT + (maxHeight * 2)
+			BACK = false
+		if !BACK && position.x >= MAXHEIGHT:
+			position.x = MAXHEIGHT + 1
+			MAXHEIGHT = MAXHEIGHT - (maxHeight * 2)
+			BACK = true
+	else: 
+		position.y = move_toward(position.y,MAXHEIGHT, speed*delta)
+		if BACK && position.y <= MAXHEIGHT:
+			position.y = MAXHEIGHT + 1
+			MAXHEIGHT = MAXHEIGHT + (maxHeight * 2)
+			BACK = false
+		if !BACK && position.y >= MAXHEIGHT:
+			position.y = MAXHEIGHT - 1
+			MAXHEIGHT = MAXHEIGHT - (maxHeight * 2)
+			BACK = true
